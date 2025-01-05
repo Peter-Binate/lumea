cb272c3914c6702889eb859e2e2cec9e7a4933d0
Peter-Binate <peter.binate@gmail.com>
2025-01-05 17:13:45 +0100

    fix: change architecture files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import FoodCard from '@/app/components/cards/food/FoodCard';
 import { mockFoodCardProps } from '@/app/components/cards/food/FoodCard.mocks';
 import PrimaryLayout from '@/app/components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '@/app/components/layouts/sidebar/SidebarLayout';
-import { NextPageWithLayout } from '@/app/page';
 import styles from '@/app/page.module.css';
+import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
